235d758fe58858c83837f294607079a795495ca2
Tom Ihde <tydie@hotmail.com>
2024-11-25 22:36:36 -0500

    add inputs folder

---
+++ inputs/forcisets/TSS/totalSuspSolidsHeaders.ts
@@ -0,0 +1,20 @@
+# Suspended sediment fluxes from the subcatchment
+# For Chesapeak Atlantis Dec 2 2011
+# Sed flux (tss, total susp solids) flux is based on Water Quality model output from Jing Wu and Gary Shenk, EPA
+# Note the row below indicating the day the Atlantis model starts. 
+#
+# Isaac Kaplan isaac.kaplan@noaa.gov
+#  
+#  
+## COLUMNS 2
+## 
+## COLUMN1.name Time
+## COLUMN1.long_name Time
+## COLUMN1.units days since 1950-01-01 0:00:00 +10
+## COLUMN1.missing_value -999
+##
+## COLUMN2.name SED
+## COLUMN2.long_name Total Suspended Solids
+## COLUMN2.units g/s
+## COLUMN2.missing_value -999
+##
---
+++ inputs/forcisets/nutloads/nutHeaders.ts
@@ -0,0 +1,39 @@
+# This is a  nutrient forcing data file,
+# For Chesapeak Atlantis March 8 2011
+# Nutriet flux is based on Water Quality model output from Jing Wu and Gary Shenk, EPA
+# Note the row below indicating the day the Atlantis model starts. 
+#
+# Isaac Kaplan isaac.kaplan@noaa.gov
+#  
+## COLUMNS 6
+## 
+## COLUMN1.name Time
+## COLUMN1.long_name Time
+## COLUMN1.units days since 1950-01-01 0:00:00 +10
+## COLUMN1.missing_value -999
+##
+## COLUMN2.name NH3
+## COLUMN2.long_name NH3
+## COLUMN2.units mg s-1
+## COLUMN2.missing_value -999
+#
+## COLUMN3.name NO3
+## COLUMN3.long_name NO3
+## COLUMN3.units mg s-1
+## COLUMN3.missing_value -999
+##
+## COLUMN4.name Lab_Det_N
+## COLUMN4.long_name Lab_Sed_N
+## COLUMN4.units mg s-1
+## COLUMN4.missing_value -999
+##
+## COLUMN5.name Si
+## COLUMN5.long_name Silica
+## COLUMN5.units mg s-1
+## COLUMN5.missing_value -999
+##
+## COLUMN6.name Det_Si
+## COLUMN6.long_name Detrital Silica
+## COLUMN6.units mg s-1
+## COLUMN6.missing_value -999
+##
